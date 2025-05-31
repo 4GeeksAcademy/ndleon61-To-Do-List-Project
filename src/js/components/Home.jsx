8d0aa9556eff35c1b42db7f99c923b11a907ff5a
David Leon <ndleon61@gmail.com>
2025-05-31 02:37:51 +0000

    Started working on this

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,48 +1,56 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import './home.css';
 
+const username = "david_leon"
 
-//create your first component
 const Home = () => {
-	const [tasks, setTasks] = useState([]);
-	const [input, setInput] = useState("");
-
-	const addTask = () =>{
-		if (input.trim() !==""){
-			setTasks([...tasks, {id: Date.now(), text:input}]);
-			setInput("");
-		}
-	};
-
-	const deleteTask = (id) => {
-		setTasks(tasks.filter(task => task.id !== id));
-	};
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
 
 
-	return (
-		<div className="container">
-			<h1>TO-DO LIST <i class="fa-solid fa-square-check"></i></h1>
+  useEffect( () => {
 
-			<div className="input">
-				<input 
-					type="text" 
-					className="tasks-container"
-					placeholder='"Add a task"'
-					value={input}
-					onChange={(e) => setInput(e.target.value)} 
-				/>
-				<button className="add-button" onClick={addTask}>Add</button>
-			</div>
-			
+	fetch(`https://playground.4geeks.com/todo/users/${username}`), {
+		method: 'GET',
+	}
 
-			<ul>
-  				{tasks.map((task) => (
-    				<li key={task.id} className="tasks"><span>{task.text}</span><button className="delete-button" onClick={() => deleteTask(task.id)}>Delete</button></li>
-  				))}
-			</ul>
+  }, []);
 
-		</div>
-	);
+  
+  const addTask = () => {
+  
+  };
+
+ 
+  const deleteTask = () => {
+  
+  };
+
+  return (
+    <div className="container">
+      <h1>TO-DO LIST <i className="fa-solid fa-square-check"></i></h1>
+
+      <div className="input">
+        <input 
+          type="text" 
+          className="tasks-container"
+          placeholder="Add a task"
+          value={input}
+          onChange={(e) => setInput(e.target.value)} 
+        />
+        <button className="add-button" onClick={addTask}>Add</button>
+      </div>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id} className="tasks">
+            <span>{task.text}</span>
+            <button className="delete-button" onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
