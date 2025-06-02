0991d35e37212374dc6479da878801c0a52a978a
David Leon <ndleon61@gmail.com>
2025-06-02 01:04:32 +0000

    Second Commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,59 @@
 import React, { useEffect, useState } from "react";
 import './home.css';
 
-const username = "david_leon"
+const user_name = "david_leon"
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState("");
 
+  const loadTasks = async () => {
+    try {
+      const res = await fetch(`https://playground.4geeks.com/todo/users`);
+      if (res.status === 404) {
+        console.log("User not found. Creating user david_leon");
+        const createRes = await fetch (`https://playground.4geeks.com/todo/users/${user_name}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application.json'
+          },
+          body: JSON.stringify
+        });
+        if (!createRes.ok) throw new Error("Failed to create user");
+        return loadTasks();
+      }
+      const data = await res.json();
+      setTasks(data);
+    } catch (err) {
+      console.error("Error loading tasks:", err);
+    }
+  };
 
-  useEffect( () => {
-
-	fetch(`https://playground.4geeks.com/todo/users/${username}`), {
-		method: 'GET',
-	}
-
+  useEffect(() => {
+    loadTasks();
   }, []);
+
+
+
+
+
+  /*useEffect( () => {
+
+    fetch(`https://playground.4geeks.com/todo/users/${user_name}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    })
+
+  }, []);*/
 
   
   const addTask = () => {
@@ -41,13 +80,17 @@ const Home = () => {
         <button className="add-button" onClick={addTask}>Add</button>
       </div>
 
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id} className="tasks">
-            <span>{task.text}</span>
-            <button className="delete-button" onClick={() => deleteTask(task.id)}>Delete</button>
-          </li>
-        ))}
+     <ul>
+        {Array.isArray(tasks) ? (
+          tasks.map((task) => (
+            <li key={task.id} className="tasks">
+              <span>{task.label}</span>
+              <button className="delete-button" onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))
+        ) : (
+          <li>No tasks found</li>
+        )}
       </ul>
     </div>
   );
